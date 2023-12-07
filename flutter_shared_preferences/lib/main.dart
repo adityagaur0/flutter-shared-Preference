e27fb42c42149b073b37deb388e371cc030c3713
adityagaur0 <adityakumargaur0@gmail.com>
2023-12-07 12:03:34 +0000

    7 Dec 2023, 17:33

--- flutter_shared_preferences/lib/main.dart
+++ flutter_shared_preferences/lib/main.dart
@@ -1,20 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final TextEditingController textEditingController = TextEditingController();
+  static const String KEYNAME = "name";
+  var namevalue = "No value saved";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getValue();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text("Shared Preference"),
+        ),
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+              child: TextFormField(
+                controller: textEditingController,
+                decoration: InputDecoration(
+                    label: Text("Name"),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(
+                        width: 2,
+                      ),
+                    )),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  var name = textEditingController.text.toString();
+                  // Obtain shared preferences.
+                  var prefs = await SharedPreferences.getInstance();
+                  // Save an String value to 'action' key.
+                  await prefs.setString(KEYNAME, name);
+                  // Update the value after saving to SharedPreferences.
+                  getValue();
+                },
+                child: const Text("Save")),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(namevalue),
+          ]),
         ),
       ),
     );
+  }
+
+  void getValue() async {
+    // Obtain shared preferences.
+    var prefs = await SharedPreferences.getInstance();
+    var getName = prefs.getString(KEYNAME);
+    namevalue = getName ?? "No Value Saved";
+    setState(() {});
   }
 }
